9cdb8d8464eeff7b4adb58d605bacaccf2b2b798
Addison Wanlass <hi@addison.codes>
2023-03-02 23:56:45 -0500

    feat: initialize project

---
+++ lib/notion-api.ts
@@ -0,0 +1,8 @@
+import { NotionAPI } from 'notion-client'
+
+export const notion = new NotionAPI({
+  // authToken: process.env.NOTION_TOKEN_V2,
+  // activeUser: process.env.NOTION_ACTIVE_USER
+  authToken: process.env.NOTION_TOKEN_V2,
+  activeUser: process.env.NOTION_ACTIVE_USER
+})
