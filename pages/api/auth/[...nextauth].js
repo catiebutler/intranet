a327b89d77066454d29b8f1718785defbe0756f7
Addison Wanlass <hi@addison.codes>
2023-06-14 00:24:40 -0400

    fix: patch npm

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,9 +9,19 @@ export const authOptions = {
       clientId: process.env.AZURE_AD_CLIENT_ID,
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
       tenantId: process.env.AZURE_AD_TENANT_ID,
+      authorization: { params: { scope: "openid profile user.Read email" } },
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      // IMPORTANT: Persist the access_token to the token right after sign in
+      if (account) {
+        token.idToken = account.id_token;
+      }
+      return token;
+    },
+  },
 }
 
 export default NextAuth(authOptions)
