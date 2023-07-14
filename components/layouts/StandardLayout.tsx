716041e5e1be6e11247b228aea37b476c2a4077a
Addison Wanlass <hi@addison.codes>
2023-07-14 13:29:04 -0400

    fix: header

--- components/layouts/StandardLayout.tsx
+++ components/layouts/StandardLayout.tsx
@@ -93,42 +93,29 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
   return (
     <>
 <header className={"flex flex-col "}>
-<nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
-  <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
-    <a href="https://flowbite.com" className="flex items-center">
-      <img
-        src="https://flowbite.com/docs/images/logo.svg"
-        className="h-8 mr-3"
-        alt="Flowbite Logo"
-      />
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-        Flowbite
-      </span>
-    </a>
-    <button
-      data-collapse-toggle="mega-menu-full-image"
-      type="button"
-      className="inline-flex items-center justify-center w-10 h-10 p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-      aria-controls="mega-menu-full-image"
-      aria-expanded="false"
-    >
-      <span className="sr-only">Open main menu</span>
-      <svg
-        className="w-5 h-5"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 17 14"
-      >
-        <path
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M1 1h15M1 7h15M1 13h15"
-        />
-      </svg>
-    </button>
+
+  <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-900 dark:border-gray-800 order-1 border-b">
+    <div className="flex items-center justify-between">
+      <div className="flex items-center justify-start flex-shrink-0">
+        <a href="/" className="flex mr-6">
+          <img src="/aptp-logo.png" className="h-16 mr-3" alt="Alliance PTP Logo" />
+          {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Alliance PTP</span> */}
+        </a>
+      </div>
+      <Dropdown />
+
+      <div className="flex items-center justify-between flex-shrink-0 ml-4 sm:hidden lg:order-2">
+
+
+        <button type="button" id="toggleMobileMenuButton" data-collapse-toggle="toggleMobileMenu" className="items-center p-2 text-gray-500 rounded-lg md:ml-2 md:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
+          <span className="sr-only">Open menu</span>
+          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
+        </button>
+      </div>
+    </div>
+  </nav>
+  <nav id="toggleMobileMenu" className="order-3 hidden bg-white border-b border-gray-200 shadow-sm dark:bg-gray-900 md:block dark:border-gray-800 md:order-2">
+    <div ref={wrapperRef} className="px-4 py-3 lg:px-6">
     <div
       id="mega-menu-full-image"
       className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
@@ -136,7 +123,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
       <ul className="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
         <li>
           <a
-            href="#"
+            href="/"
             className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
             aria-current="page"
           >
@@ -149,7 +136,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
             data-collapse-toggle="mega-menu-full-image-dropdown"
             className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
           >
-            Company{" "}
+            Our Teams{" "}
             <svg
               className="w-2.5 h-2.5 ml-2.5"
               aria-hidden="true"
@@ -168,29 +155,14 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
           </button>
         </li>
         <li>
-          <a
-            href="#"
-            className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            Marketplace
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            Resources
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            Contact
-          </a>
-        </li>
+            <a href="/news" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Company News</a>
+          </li>
+          <li>
+            <a href="/connect" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Stay Connected</a>
+          </li>
+          <li>
+            <a href="/jobs" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Open Jobs</a>
+          </li>
       </ul>
     </div>
   </div>
@@ -208,7 +180,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
             href="#"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
-            Online Stores
+            Front Office
           </a>
         </li>
         <li>
@@ -216,7 +188,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
             href="#"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
-            Segmentation
+            Marketing
           </a>
         </li>
         <li>
@@ -224,7 +196,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
             href="#"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
-            Marketing CRM
+            Clinical Operations
           </a>
         </li>
         <li>
@@ -232,7 +204,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
             href="#"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
-            Online Stores
+            Information Technology
           </a>
         </li>
       </ul>
@@ -242,7 +214,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
             href="#"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
-            Our Blog
+            Human Resources
           </a>
         </li>
         <li>
@@ -250,25 +222,10 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
             href="#"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
-            Terms &amp; Conditions
+            Continuing Education
           </a>
         </li>
-        <li>
-          <a
-            href="#"
-            className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
-          >
-            License
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
-          >
-            Resources
-          </a>
-        </li>
+
       </ul>
       <a
         href="#"
@@ -276,7 +233,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
         style={{ backgroundImage: "url(/docs/images/dashboard-overview.png)" }}
       >
         <p className="max-w-xl mb-5 font-extrabold leading-tight tracking-tight text-white">
-          Preview the new Flowbite dashboard navigation.
+          Big callout here.
         </p>
         <button
           type="button"
@@ -301,48 +258,10 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
         </button>
       </a>
     </div>
-  </div>
-</nav>
 
-  <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-900 dark:border-gray-800 order-1 border-b">
-    <div className="flex items-center justify-between">
-      <div className="flex items-center justify-start flex-shrink-0">
-        <a href="/" className="flex mr-6">
-          <img src="/aptp-logo.png" className="h-16 mr-3" alt="Alliance PTP Logo" />
-          {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Alliance PTP</span> */}
-        </a>
-      </div>
-      <Dropdown />
-
-      <div className="flex items-center justify-between flex-shrink-0 ml-4 sm:hidden lg:order-2">
-
-
-        <button type="button" id="toggleMobileMenuButton" data-collapse-toggle="toggleMobileMenu" className="items-center p-2 text-gray-500 rounded-lg md:ml-2 md:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
-          <span className="sr-only">Open menu</span>
-          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
-        </button>
-      </div>
-    </div>
-  </nav>
-  <nav id="toggleMobileMenu" className="order-3 hidden bg-white border-b border-gray-200 shadow-sm dark:bg-gray-900 md:block dark:border-gray-800 md:order-2">
-    <div ref={wrapperRef} className="px-4 py-3 lg:px-6">
       <div className="flex flex-col items-center justify-between md:flex-row">
         <ul className="flex flex-col order-2 w-full mt-0 text-sm font-medium border border-gray-200 rounded-lg bg-gray-50 md:flex-row md:order-1 md:bg-white dark:bg-gray-800 dark:border-gray-700 dark:md:bg-gray-900 md:rounded-none md:border-0">
-          <li>
-            <a href="/" className="block px-4 py-3 text-gray-500 rounded-lg dark:text-gray-400 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700" aria-current="page">Home</a>
-          </li>
-          <li>
-            <a href="/teams" className="block px-4 py-3 text-gray-500 rounded-lg dark:text-gray-400 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">Our Teams</a>
-          </li>
-          <li>
-            <a href="/news" className="block px-4 py-3 text-gray-500 rounded-lg dark:text-gray-400 hover:text-gray-900 dark:hover:text-white dark:hover:bg-gray-800 hover:bg-gray-50 dark:hover-bg-gray-800 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">Company News</a>
-          </li>
-          <li>
-            <a href="/connect" className="block px-4 py-3 text-gray-500 rounded-lg dark:text-gray-400 hover:text-gray-900 dark:hover:text-white dark:hover:bg-gray-800 hover:bg-gray-50 dark:hover-bg-gray-800 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">Stay Connected</a>
-          </li>
-          <li>
-            <a href="/jobs" className="block px-4 py-3 text-gray-500 rounded-lg dark:text-gray-400 hover:text-gray-900 dark:hover:text-white dark:hover:bg-gray-800 hover:bg-gray-50 dark:hover-bg-gray-800 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700">Open Jobs</a>
-          </li>
+
           {/* TODO: Add role */}
           {session?.user.name === 'admin' ? (
           <li>
