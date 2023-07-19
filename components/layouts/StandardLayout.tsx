bffaa08debc6f53af74349890810390df81df1c3
Addison Wanlass <hi@addison.codes>
2023-07-19 12:11:15 -0400

    fix: mega menu links and layout

--- components/layouts/StandardLayout.tsx
+++ components/layouts/StandardLayout.tsx
@@ -164,8 +164,29 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
             <a href="/jobs" className="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Open Jobs</a>
           </li>
       </ul>
-    </div>
+      <div className="flex flex-col items-center justify-between md:flex-row">
+          <Search setSearchResults={setSearchResults} />
+          {searchResults?.length > 0 && (
+  <div className="absolute right-0 z-10 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg top-16">
+    <ul className="divide-y divide-gray-200">
+      {searchResults.map((result) => (
+        <li key={result.id}>
+          <a href={mapPageUrl(result.id)} className="block px-4 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100">
+            <h2 className="text-base font-medium text-gray-900">{result.title}</h2>
+
+            <p className="mt-1 text-sm leading-tight text-gray-500">{result.description}</p>
+          </a>
+        </li>
+      ))}
+    </ul>
   </div>
+)}
+    {/* <ToggleThemeButton /> */}
+
+      </div>
+    </div>
+
+    </div>
   <div
     id="mega-menu-full-image-dropdown"
     className="hidden mt-1 bg-white border-gray-200 shadow-sm border-y dark:bg-gray-800 dark:border-gray-600"
@@ -177,7 +198,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
       >
         <li>
           <a
-            href="#"
+            href="/02ddedf1-8c36-4081-86e1-e65e800ade10"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
             Front Office
@@ -185,7 +206,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
         </li>
         <li>
           <a
-            href="#"
+            href="/bd941b39-cf66-4714-9d15-a6e3acfbdec8"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
             Marketing
@@ -193,25 +214,25 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
         </li>
         <li>
           <a
-            href="#"
+            href="/a465a6ca-79d5-4213-8514-6eac0c5201a1"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
             Clinical Operations
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
-          >
-            Information Technology
           </a>
         </li>
       </ul>
       <ul className="mb-4 space-y-4 md:mb-0">
         <li>
           <a
-            href="#"
+            href="/e7a11eef-9544-4dee-9893-23c29b4176b0"
+            className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
+          >
+            Information Technology
+          </a>
+        </li>
+        <li>
+          <a
+            href="/f4f28e1f-4c45-453d-8fad-c175fffb049a"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
             Human Resources
@@ -219,7 +240,7 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
         </li>
         <li>
           <a
-            href="#"
+            href="/6665d94f-9e3b-41b0-96b8-8737af9b7a6a"
             className="hover:underline hover:text-blue-600 dark:hover:text-blue-500"
           >
             Continuing Education
@@ -258,28 +279,8 @@ export const StandardLayout: React.FC<React.PropsWithChildren> = ({ children }) 
         </button>
       </a>
     </div>
-
-      <div className="flex flex-col items-center justify-between md:flex-row">
-          <Search setSearchResults={setSearchResults} />
-          {searchResults?.length > 0 && (
-  <div className="absolute right-0 z-10 w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg top-16">
-    <ul className="divide-y divide-gray-200">
-      {searchResults.map((result) => (
-        <li key={result.id}>
-          <a href={mapPageUrl(result.id)} className="block px-4 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100">
-            <h2 className="text-base font-medium text-gray-900">{result.title}</h2>
-
-            <p className="mt-1 text-sm leading-tight text-gray-500">{result.description}</p>
-          </a>
-        </li>
-      ))}
-    </ul>
-  </div>
-)}
-    {/* <ToggleThemeButton /> */}
-
-      </div>
     </div>
+
   </nav>
 
 </header>
