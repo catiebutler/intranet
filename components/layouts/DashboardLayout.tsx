68c48ea6e169de80572429ce2203a9eda54ccd52
Addison Wanlass <hi@addison.codes>
2023-07-24 00:13:46 -0400

    build: res

--- components/layouts/DashboardLayout.tsx
+++ components/layouts/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Announcements from '@/components/Announcenments'
+// import Announcements from '@/components/Announcenments'
 import Sidebar from '@/components/Sidebar'
 import SocialFeed from '@/components/SocialFeed'
 import { useRouter } from 'next/router'
@@ -13,7 +13,7 @@ export const DashboardLayout: React.FC<React.PropsWithChildren> = ({ children })
     return (
       <div className='container mx-auto'>
         <div className="container grid grid-cols-4 gap-6 pt-4 mx-auto">
-        <Announcements />
+        {/* <Announcements /> */}
         <Sidebar />
         <div className="col-span-3 mt-6">
         {children}
